8be60c43c3ad7f2a4f40a7304404af0537d914bd
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-11-28 08:05:46 -0600

    initial commit

---
+++ server.ts
@@ -0,0 +1,27 @@
+import { type Context, Hono } from "hono";
+import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
+import fastSSR from "@microsoft/fast-ssr";
+
+const { defaultRenderInfo, templateRenderer } = fastSSR();
+
+const html = String.raw;
+const app = new Hono();
+
+app.use("/*", serveStatic({ root: "." }));
+
+// This returns HTML that includes server-side rendered FAST components.
+app.get("/greet", async (c: Context) => {
+  const name = c.req.query("name");
+  const template = html`
+    <p>The following are server-side rendered FAST components.</p>
+    <hello-world></hello-world>
+    <hello-world name=${name}></hello-world>
+  `;
+  const result = templateRenderer.render(template, defaultRenderInfo);
+  return c.html(result);
+});
+
+serve(app, (info) => {
+  console.log(`listing on port ${info.port}`);
+});
