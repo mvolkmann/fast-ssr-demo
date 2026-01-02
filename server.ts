0efe147433114dcdc176d808b385344527aa5a0a
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-01-02 14:36:37 -0600

    progress

--- server.ts
+++ server.ts
@@ -1,33 +1,12 @@
 import { type Context, Hono } from "hono";
-import { stream } from "hono/streaming";
 import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
-// The next import must precede the import that follows it.
-import "@microsoft/fast-ssr/install-dom-shim.js";
-import fastSSR from "@microsoft/fast-ssr";
-
-const { templateRenderer } = fastSSR();
+import { streamHTML } from "./stream.ts";
 
 const html = String.raw;
 const app = new Hono();
 
 app.use("/*", serveStatic({ root: "." }));
-
-function streamHTML(c: Context, html: string) {
-  // The type of result is IterableIterator<string>.
-  const iterator = templateRenderer.render(html);
-
-  c.header("Content-Type", "text/html; charset=utf-8");
-  c.header("Transfer-Encoding", "chunked");
-  c.header("X-Content-Type-Options", "nosniff"); // security measure
-  const encoder = new TextEncoder();
-
-  return stream(c, async (stream) => {
-    for (const chunk of iterator) {
-      await stream.write(encoder.encode(chunk));
-    }
-  });
-}
 
 // This returns HTML that includes server-side rendered FAST components.
 app.get("/greet", async (c: Context) => {
---
+++ stream.ts
@@ -0,0 +1,23 @@
+import { type Context } from "hono";
+import { stream } from "hono/streaming";
+// The next import must precede the import that follows it.
+import "@microsoft/fast-ssr/install-dom-shim.js";
+import fastSSR from "@microsoft/fast-ssr";
+
+const { templateRenderer } = fastSSR();
+
+export function streamHTML(c: Context, html: string) {
+  // The type of result is IterableIterator<string>.
+  const iterator = templateRenderer.render(html);
+
+  c.header("Content-Type", "text/html; charset=utf-8");
+  c.header("Transfer-Encoding", "chunked");
+  c.header("X-Content-Type-Options", "nosniff"); // security measure
+  const encoder = new TextEncoder();
+
+  return stream(c, async (stream) => {
+    for (const chunk of iterator) {
+      await stream.write(encoder.encode(chunk));
+    }
+  });
+}
