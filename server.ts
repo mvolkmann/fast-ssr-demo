b5f64579497c2f606507244b7d571a25c1483ce7
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-01-02 14:33:15 -0600

    finally got FAST SSR working!

--- server.ts
+++ server.ts
@@ -1,16 +1,33 @@
 import { type Context, Hono } from "hono";
+import { stream } from "hono/streaming";
 import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
+// The next import must precede the import that follows it.
 import "@microsoft/fast-ssr/install-dom-shim.js";
 import fastSSR from "@microsoft/fast-ssr";
 
-const { defaultRenderInfo, templateRenderer } = fastSSR();
-console.log("server.ts : defaultRenderInfo =", defaultRenderInfo);
+const { templateRenderer } = fastSSR();
 
 const html = String.raw;
 const app = new Hono();
 
 app.use("/*", serveStatic({ root: "." }));
+
+function streamHTML(c: Context, html: string) {
+  // The type of result is IterableIterator<string>.
+  const iterator = templateRenderer.render(html);
+
+  c.header("Content-Type", "text/html; charset=utf-8");
+  c.header("Transfer-Encoding", "chunked");
+  c.header("X-Content-Type-Options", "nosniff"); // security measure
+  const encoder = new TextEncoder();
+
+  return stream(c, async (stream) => {
+    for (const chunk of iterator) {
+      await stream.write(encoder.encode(chunk));
+    }
+  });
+}
 
 // This returns HTML that includes server-side rendered FAST components.
 app.get("/greet", async (c: Context) => {
@@ -20,8 +37,7 @@ app.get("/greet", async (c: Context) => {
     <hello-world></hello-world>
     <hello-world name=${name}></hello-world>
   `;
-  const result = templateRenderer.render(template, defaultRenderInfo);
-  return c.html(result);
+  return streamHTML(c, template);
 });
 
 serve(app, (info) => {
